68e704a162448eac71e2771f201f1570841edcde
schedges <shedges@gmail.com>
2020-10-15 05:04:11 +0000

    Adding capability to use html arguments to generate a search, still need to do more testing

--- loiSearcher_main.py
+++ loiSearcher_main.py
@@ -47,3 +47,70 @@ def loiSearch_page():
         </html>
     '''.format(errors=errors)
 
+###html search section###
+from flask import jsonify
+import pickle
+
+inpFilename="/home/shedges/mysite/LOI_dict.pickle"
+f = open(inpFilename, 'rb')
+
+#Load loiList, {id,frontier,filename,link,text}
+loiList = pickle.load(f)
+
+#List of frontiers
+frontiers=["AF","CF","CommF","CompF","EF","IF","NF","RF","TF","UF"]
+
+@app.route('/search', methods=['GET'])
+def api_id():
+    #We don't want to modify the list if we restrict to a frontier,
+    #so make a copy of it
+    localList=loiList
+
+    #Deal with frontier argument, if present
+    frontier=""
+    if 'frontier' in request.args:
+        #Get the requested frontier
+        frontier = request.args['frontier']
+        #Check the frontier is valid
+        if frontier in frontiers:
+          #Reduce dictionary to subset matching request
+          localList = [i for i in localList if i["frontier"]==frontier]
+
+    #Parse the case sensitive argument, if present
+    caseSensitive=0
+    if 'caseSensitive' in request.args:
+      caseSensitive=1
+
+    #Holds the results from the search
+    results=[]
+    nTimes=0
+
+    #Parse the search term, if there
+    if 'term' in request.args:
+      term = request.args['term']
+    else:
+      term=""
+
+    #Do the search
+    if caseSensitive==0:
+      for loi in localList:
+        nTimes = loi['text'].count(term)
+        if nTimes>0:
+          loi['nTimes']=str(nTimes)
+          results.append(loi)
+    else:
+      for loi in localList:
+        nTimes = loi['text'].lower().count(term.lower())
+        if nTimes>0:
+          loi['nTimes']=str(nTimes)
+          results.append(loi)
+
+
+    #Sort by number of occurrences of the search word
+    results.sort(key = lambda x: int(x['nTimes']))
+    results.reverse()
+
+    # Use the jsonify function from Flask to convert our list of
+    # Python dictionaries to the JSON format.
+    return jsonify(results)
+
